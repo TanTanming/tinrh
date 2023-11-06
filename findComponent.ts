6cf06516c046cab7e70a0cf14a1839ac47878af0
赵新明 <zhaoxm@tuyuansu.com.cn>
2023-11-06 14:37:11 +0800

    微件逻辑

---
+++ findComponent.ts
@@ -0,0 +1,25 @@
+interface IObject {
+  [key: string]: any;
+}
+
+export const findComponent = (componentName: string) => {
+  let files: IObject;
+  if (typeof (import.meta as IObject).env === 'object') {
+    console.log('使用Vite');
+    files = (import.meta as IObject).glob('@/wigets/**/index.vue', {
+      eager: true,
+    });
+  } else {
+    console.log('使用Webpack或未知构建工具');
+    files = (require as any)?.context('@/wigets', true, /index.vue$/);
+  }
+
+  let c;
+  Object.keys(files).forEach((fileName) => {
+    const componentConfig: IObject = files[fileName];
+    if (componentConfig.default.name === componentName) {
+      c = componentConfig.default || componentConfig;
+    }
+  });
+  return c;
+};
---
+++ index.ts
@@ -0,0 +1,4 @@
+import { openWidget, closeWidget, getActiveWidgets, deepClone } from './widegt';
+import { findComponent } from './findComponent';
+
+export { openWidget, closeWidget, getActiveWidgets, deepClone, findComponent };
