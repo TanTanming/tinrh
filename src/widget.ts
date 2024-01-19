c311f19537529188b5d53f8f60d8633c5f4d7cb4
赵新明 <zhaoxm@tuyuansu.com.cn>
2024-01-19 13:54:25 +0800

    bug修复

--- src/widget.ts
+++ src/widget.ts
@@ -3,16 +3,14 @@ import { nanoid } from 'nanoid';
 import { findComponent } from './findComponent';
 type Component = import('vue').DefineComponent<{}, {}, any>;
 
-const widgetsObj: Record<string, any> = ref({});
+const widgetsObj: Record<string, any> | null = ref({});
 const files: any = ref(null);
 
 export const setWidgetFile = (f: any[]) => {
   files.value = f;
 };
 
-export const getWidgetInfo = () => {
-  return files.value;
-};
+export const getWidgetInfo = () => files.value;
 
 export const openWidget = (
   componentName: string,
@@ -21,17 +19,21 @@ export const openWidget = (
 ) => {
   const targetElement: HTMLElement | null =
     document.getElementById(targetSelector);
-  const c: Component = findComponent(componentName)!;
-  const widget = deepClone(c);
-  widget.parent = targetElement;
-  options.closeId = nanoid();
-
   if (!targetElement) {
     console.error(
       `Target element with selector '${targetSelector}' not found.`
     );
     return;
   }
+  const c: Component = findComponent(componentName)!;
+  if (!c) {
+    console.error(`Component '${componentName}' not found.`);
+    return;
+  }
+  const widget = deepClone(c);
+  widget.parent = targetElement;
+  options.closeId = nanoid();
+
   const div: HTMLElement = document.createElement('div');
   div.setAttribute('id', options.closeId);
   widget.closeId = options.closeId;
@@ -43,23 +45,30 @@ export const openWidget = (
   });
 
   app.mount(div);
+  console.log(`Widget '${componentName}' 挂载成功.`);
   widgetsObj.value[options.closeId] = widget;
+  return {
+    closeId: widget,
+  };
 };
 
 export const closeWidget = (key: string) => {
+  if (!widgetsObj.value || !widgetsObj.value[key]) return;
   widgetsObj.value[key].parent?.removeChild(document.getElementById(key));
   delete widgetsObj.value[key];
+  console.log(`Widget '${key}' 卸载成功.`);
 };
 
 export const closeAllWidgets = () => {
+  if (!widgetsObj.value) return;
   Object.keys(widgetsObj.value).forEach((key: string) => {
     widgetsObj.value[key].parent?.removeChild(document.getElementById(key));
   });
   widgetsObj.value = {};
+  console.log('All widgets are closed.');
 };
-export const getActiveWidgets = () => {
-  return widgetsObj.value;
-};
+
+export const getActiveWidgets = () => widgetsObj.value;
 
 export const deepClone = (obj: Record<string, any>) => {
   const result: any = Array.isArray(obj) ? [] : {};
